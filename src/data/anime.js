48682c498ae278e8af8faad156171ff290be4a85
Beksultan <beksultan.sagnaev@gmail.com>
2022-07-13 00:53:30 +0600

    init: first commit

---
+++ src/data/anime.js
@@ -0,0 +1,252 @@
+const animeList = [
+    {
+        name: "Наруто",
+        time: 270,
+        time2: "270 hours",
+    },
+    {
+        name: "Аватар",
+        time: 24,
+        time2: "24 hours",
+    },
+    {
+        name: "Убийца Акамэ",
+        time: 9.2,
+        time2: "9 hours",
+    },
+    {
+        name: "Ангельские Ритмы",
+        time: 5,
+        time2: "5 hours",
+    },
+    {
+        name: "Атака Титанов",
+        time: 33.4,
+        time2: "33 hours",
+    },
+    {
+        name: "Волейбол",
+        time: 34.5,
+        time2: "34 hours",
+    },
+    {
+        name: "Баскетбол Куроко",
+        time: 31,
+        time2: "31 hours",
+    },
+    {
+        name: "Безумный Азарт",
+        time: 9.2,
+        time2: "9 hours",
+    },
+    {
+        name: "Bleach",
+        time: 140.3,
+        time2: "140 hours",
+    },
+    {
+        name: "Ванпанчмен",
+        time: 12,
+        time2: "12 hours",
+    },
+    {
+        name: "Ван Пис",
+        time: 385,
+        time2: "385 hours",
+    },
+    {
+        name: "Вайолет Эвергарден",
+        time: 8,
+        time2: "8 hours",
+    },
+    {
+        name: "Врата Штейна",
+        time: 20.5,
+        time2: "20 hours",
+    },
+    {
+        name: "Госпажа Кагуя",
+        time: 15.2,
+        time2: "15 hours",
+    },
+    {
+        name: "Герой щит",
+        time: 14.5,
+        time2: "14 hours",
+    },
+    {
+        name: "Этот глупый свин не понимает мечту девочки-зайки",
+        time: 6,
+        time2: "6 hours",
+    },
+    {
+        name: "Город в котором меня нет",
+        time: 5,
+        time2: "5 hours",
+    },
+    {
+        name: "Дневник будущего",
+        time: 10.3,
+        time2: "10 hours",
+    },
+    {
+        name: "Доктор Стоун",
+        time: 14,
+        time2: "14 hours",
+    },
+    {
+        name: "Другая",
+        time: 5,
+        time2: "5 hours",
+    },
+    {
+        name: "Евангелион",
+        time: 16,
+        time2: "16 hours",
+    },
+    {
+        name: "За гранью",
+        time: 6,
+        time2: "6 hours",
+    },
+    {
+        name: "Ковбой Бибоп",
+        time: 10,
+        time2: "10 hours",
+    },
+    {
+        name: "Класс убийц",
+        time: 19,
+        time2: "19 hours",
+    },
+    {
+        name: "Клинок рассекающий демонов",
+        time: 18,
+        time2: "18 hours",
+    },
+    {
+        name: "Код гиас",
+        time: 35,
+        time2: "35 hours",
+    },
+    {
+        name: "Магическая битва",
+        time: 9,
+        time2: "9 hours",
+    },
+    {
+        name: "Мастера меча онлайн",
+        time: 54,
+        time2: "54 hours",
+    },
+    {
+        name: "Милый во Франксе",
+        time: 9,
+        time2: "9 hours",
+    },
+    {
+        name: "Моб Писхо 100",
+        time: 9,
+        time2: "9 hours",
+    },
+    {
+        name: "Может я встречу тебя в подземелье",
+        time: 19,
+        time2: "19 hours",
+    },
+    {
+        name: "Моя геройская академия",
+        time: 42,
+        time2: "42 hours",
+    },
+    {
+        name: "Обещенный неверланд",
+        time: 9,
+        time2: "9 hours",
+    },
+    {
+        name: "Нет игры нет жизни",
+        time: 6,
+        time2: "6 hours",
+    },
+    {
+        name: "О моем перерождений в слизь",
+        time: 21,
+        time2: "21 hours",
+    },
+    {
+        name: "Пособие по выживанию",
+        time: 20,
+        time2: "20 hours",
+    },
+    {
+        name: "Притворная любовь",
+        time: 12,
+        time2: "12 hours",
+    },
+    {
+        name: "Розовая пора",
+        time: 14,
+        time2: "14 hours",
+    },
+    {
+        name: "Сделано в бездне",
+        time: 11,
+        time2: "11 hours",
+    },
+    {
+        name: "Семь смертных грехов",
+        time: 37,
+        time2: "37 hours",
+    },
+    {
+        name: "Семья шпионов",
+        time: 5,
+        time2: "5 hours",
+    },
+    {
+        name: "Повар боец Сома",
+        time: 38,
+        time2: "38 hours",
+    },
+    {
+        name: "Стальной Алхимик",
+        time: 42,
+        time2: "42 hours",
+    },
+    {
+        name: "Токийские мстители",
+        time: 9,
+        time2: "9 hours",
+    },
+    {
+        name: "Гуль",
+        time: 23,
+        time2: "23 hours",
+    },
+    {
+        name: "Убийца гоблинов",
+        time: 5,
+        time2: "5 hours",
+    },
+    {
+        name: "Фейри тейл",
+        time: 128,
+        time2: "128 hours",
+    },
+    {
+        name: "Хантер",
+        time: 58,
+        time2: "58 hours",
+    },
+    {
+        name: "Хьека",
+        time: 8,
+        time2: "8 hours",
+    },
+    {
+        name: "Черный клевер",
+        time: 65,
+        time2: "65 hours",
+    },
+];
---
+++ src/data/serials.js
@@ -0,0 +1,66 @@
+const serialList = [
+    {
+        name: "Sherlock Holmes",
+        time: 20,
+    },
+    {
+        name: "Riverdale",
+        time: 85,
+    },
+    {
+        name: "la casa de papel",
+        time: 34,
+    },
+    {
+        name: "Strangers things",
+        time: 37,
+    },
+    {
+        name: "Queen's Gambit",
+        time: 7,
+    },
+    {
+        name: "Umbrella academy",
+        time: 30,
+    },
+    {
+        name: "Supernaturals",
+        time: 207,
+    },
+    {
+        name: "Marco Polo",
+        time: 21,
+    },
+    {
+        name: "Russian Doll",
+        time: 7,
+    },
+    {
+        name: "Lupin",
+        time: 7,
+    },
+    {
+        name: "Squid Game",
+        time: 9,
+    },
+    {
+        name: "Breaking Bad",
+        time: 50,
+    },
+    {
+        name: "Unfortuante Events",
+        time: 20,
+    },
+    {
+        name: "Snowflake Mountain",
+        time: 5,
+    },
+    {
+        name: "Man vs Bee",
+        time: 2,
+    },
+    {
+        name: "Kominsky method",
+        time: 11,
+    },
+];
