56cc0fa7599382d8787af63fb8e4cb363c7936a0
Beksultan <beksultan.sagnaev@gmail.com>
2022-07-13 03:24:42 +0600

    fix: images name, array films, netflix

--- src/data/serials.js
+++ src/data/serials.js
@@ -1,66 +1,147 @@
 const serialList = [
     {
-        name: "Sherlock Holmes",
-        time: 20,
+        "name": "Sherlock Holmes",
+        "time": 20,
+        "src": "../images/netflix/sherlock.jpg"
     },
     {
-        name: "Riverdale",
-        time: 85,
+        "name": "Riverdale",
+        "time": 85,
+        "src": "../images/netflix/riverdale.jpg"
     },
     {
-        name: "la casa de papel",
-        time: 34,
+        "name": "la casa de papel",
+        "time": 34,
+        "src": "../images/netflix/lacasa.jpg"
     },
     {
-        name: "Strangers things",
-        time: 37,
+        "name": "Strangers things",
+        "time": 37,
+        "src": "../images/netflix/stranger.jpg"
     },
     {
-        name: "Queen's Gambit",
-        time: 7,
+        "name": "Queen's Gambit",
+        "time": 7,
+        "src": "../images/netflix/queens.jpg"
     },
     {
-        name: "Umbrella academy",
-        time: 30,
+        "name": "Umbrella academy",
+        "time": 30,
+        "src": "../images/netflix/umbrella.jpg"
     },
     {
-        name: "Supernaturals",
-        time: 207,
+        "name": "Supernaturals",
+        "time": 207,
+        "src": "../images/netflix/supernat.jpg"
     },
     {
-        name: "Marco Polo",
-        time: 21,
+        "name": "Marco Polo",
+        "time": 21,
+        "src": "../images/netflix/marco.jpg"
     },
     {
-        name: "Russian Doll",
-        time: 7,
+        "name": "Russian Doll",
+        "time": 7,
+        "src": "../images/netflix/rus.jpg"
     },
     {
-        name: "Lupin",
-        time: 7,
+        "name": "Lupin",
+        "time": 7,
+        "src": "../images/netflix/lupin.jpg"
     },
     {
-        name: "Squid Game",
-        time: 9,
+        "name": "Squid Game",
+        "time": 9,
+        "src": "../images/netflix/igra.jpg"
     },
     {
-        name: "Breaking Bad",
-        time: 50,
+        "name": "Breaking Bad",
+        "time": 50,
+        "src": "../images/netflix/walter.jpg"
     },
     {
-        name: "Unfortuante Events",
-        time: 20,
+        "name": "Unfortuante Events",
+        "time": 20,
+        "src": "../images/netflix/unfurton.jpg"
     },
     {
-        name: "Snowflake Mountain",
-        time: 5,
+        "name": "Snowflake Mountain",
+        "time": 5,
+        "src": "../images/netflix/mountain.jpg"
     },
     {
-        name: "Man vs Bee",
-        time: 2,
+        "name": "Man vs Bee",
+        "time": 2,
+        "src": "../images/netflix/bean.jpg"
     },
     {
-        name: "Kominsky method",
-        time: 11,
+        "name": "Kominsky method",
+        "time": 11,
+        "src": "../images/netflix/thecominski.jpg"
     },
+    {
+        "name": "Во имя мести",
+        "time": 6.45
+        "src": "../images/netflix/myname.jpg"
+    },
+    {
+        "name": "The_walking_dead",
+        "time": 110,
+        "src": "../images/netflix/walkdead.jpg"
+    },
+    {
+        "name": "The_peaky_blinders",
+        "time": 36,
+        "src": "../images/netflix/tommy.jpg"
+    },
+    {
+        "name": "kartochny_domik",
+        "time": 60,
+        "src": "../images/netflix/cards.jpg"
+    },
+    {
+        "name": "shameless",
+        "time": 122,
+        "src": "../images/netflix/shame.jpg"
+    },
+    {
+        "name": "The_politician",
+        "time": 11,
+        "src": "../images/netflix/politic.jpg"
+    },
+    {
+        "name": "Godless",
+        "time": 7,
+        "src": "../images/netflix/godless.jpg"
+    },
+    {
+        "name": "Master_na_vse_ruki",
+        "time": 16,
+        "src": "../images/netflix/master.jpg"
+    },
+    {
+        "name": "karatel",
+        "time": 22,
+        "src": "../images/netflix/karatel.jpg"
+    },
+    {
+        "name": "sexeducation",
+        "time": 20,
+        "src": "../images/netflix/sex.jpg"
+    },
+    {
+        "name": "vampire_diaries",
+        "time": 123,
+        "src": "../images/netflix/vampire.jpg"
+    },
+    {
+        "name": "narco",
+        "time": 25,
+        "src": "../images/netflix/narco.jpg"
+    },
+    {
+        "name": "haunting",
+        "time": 9,
+        "src": "../images/netflix/house.jpg"
+    }
 ];
